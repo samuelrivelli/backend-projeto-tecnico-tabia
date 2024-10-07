0bda83e5b17e78c05e5b0c75f5bac3ed672261ab
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-10-07 18:08:42 -0300

    primeiro commit (configs iniciais do docker realizadas)

---
+++ src/main/java/com/tabia/projeto_tecnico/ProjetoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.tabia.projeto_tecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTecnicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabia/projeto_tecnico/ProjetoTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabia.projeto_tecnico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTecnicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
