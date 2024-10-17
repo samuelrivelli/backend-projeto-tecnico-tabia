b1661fba5f56e72dde57e97492f31fe854b32dee
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-10-17 18:32:51 -0300

    adicao de rotas permitidas para o websocket

--- src/main/java/com/tabia/projeto_tecnico/config/WebSocketConfig.java
+++ src/main/java/com/tabia/projeto_tecnico/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Endpoint do WebSocket
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS(); // Endpoint do WebSocket
     }
 
     @Override
