cac1de398ac320a6081eb74fefc9e6b645e70149
Samuel Rivelli <samuelmagrivelli@gmail.com>
2024-10-22 23:47:25 -0300

    commit final (ou nao)

--- src/main/java/com/tabia/projeto_tecnico/service/PollService.java
+++ src/main/java/com/tabia/projeto_tecnico/service/PollService.java
@@ -14,8 +14,6 @@ import com.tabia.projeto_tecnico.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
